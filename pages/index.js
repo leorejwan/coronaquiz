19139e0cd08103efccd795c00845f4916632ef50
leo rejwan <leorejwan@gmail.com>
2021-01-25 15:10:22 -0300

    +componente QuizContainer que será o bloco onde terá o Quiz

--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,24 @@ const BackgroundImage = styled.div`
   background-position: center;
 `;
 
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
 
 export default function Home(){
   return(
     <BackgroundImage>
-      aaa
+      <QuizContainer>
+        aaa
+      </QuizContainer>
     </BackgroundImage>
   )
 }
