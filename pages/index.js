c5a69fb2a23cbf28faf15f248dcd71b3e4fa11ba
leo rejwan <leorejwan@gmail.com>
2021-01-25 15:40:59 -0300

    +codigo CSS estruturado como React. GithubCorner tem um link bonito pro seu github

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import styled from 'styled-components'
-import db from '../db.json'
-
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`;
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -19,13 +17,30 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
 export default function Home(){
   return(
-    <BackgroundImage>
+    <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-        aaa
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
       </QuizContainer>
-    </BackgroundImage>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+    </QuizBackground>
   )
 }
