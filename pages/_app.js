e5615683eddde2693cf410379b5719e3a24540db
leo rejwan <leorejwan@gmail.com>
2021-01-25 13:04:57 -0300

    Se apagar codigo CSS sem querer, em tempo de compilação vai gerar erro.

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    background-color: red;
-  }
-`
+// const GlobalStyle = createGlobalStyle`
+//   body {
+//     margin: 0;
+//     padding: 0;
+//     box-sizing: border-box;
+//     background-color: red;
+//   }
+// `
 
 const theme = {
   colors: {
