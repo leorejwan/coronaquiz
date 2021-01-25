1da58b4771888728139671ed6620f34511b334f1
leo rejwan <leorejwan@gmail.com>
2021-01-25 13:05:27 -0300

    removido sujeiras dos comits anteriores anteriores

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
-// const GlobalStyle = createGlobalStyle`
-//   body {
-//     margin: 0;
-//     padding: 0;
-//     box-sizing: border-box;
-//     background-color: red;
-//   }
-// `
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
 
 const theme = {
   colors: {
