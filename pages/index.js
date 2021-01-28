35a98520b1b0e3a123f6b157698246c7d741abd7
leo rejwan <leorejwan@gmail.com>
2021-01-28 06:44:30 -0300

    +Button estilizado com styled-components. Substituido button por Button no index

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 import Input from '../src/components/Input'
+import Button from '../src/components/Button'
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -52,9 +53,9 @@ export default function Home(){
               }}
               placeholder="Digite seu nome" />
 
-              <button type="submit" disabled={name.length === 0}>
+              <Button type="submit" disabled={name.length === 0}>
                 Jogar {name}
-              </button>
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
