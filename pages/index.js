9c903376d1450dcc625f24f2bf3a4d92219e8453
leo rejwan <leorejwan@gmail.com>
2021-01-28 07:56:20 -0300

    + Quiz. Tela de loading automatico sai para questoes. Aparecem as perguntas com as alternativas. QuizContainer em um componente separado

--- pages/index.js
+++ pages/index.js
@@ -6,21 +6,11 @@ import db from '../db.json';
 import Widget from '../src/components/Widget'
 import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
+import QuizContainer from '../src/components/QuizContainer'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
 import Input from '../src/components/Input'
 import Button from '../src/components/Button'
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home(){
   const router = useRouter()
